0e55359e1c3a41ec8de498011b104a7f3def0517
Rohan Chhipa <rohan.chhipa@live.com>
2021-07-11 08:58:05 +0200

    Rename Main to Console

---
+++ PolygonGa.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using PolygonGa.Algorithm;
+using Serilog;
+
+namespace PolygonGa.Console
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var runner = new AlgorithmRunner(logger);
+            await runner.Run("Images/polygon.png");
+        }
+    }
+}
